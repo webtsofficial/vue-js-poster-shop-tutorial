61ede334e65269221802f9fc97a042659e1f896a
Tobias Schramek <webtsofficial@gmail.com>
2018-09-26 21:52:15 +0200

    added cart functions, increase / decrease items + added submit Button + function for the search field

--- public/script.js
+++ public/script.js
@@ -4,22 +4,64 @@ new Vue({
         total: 0,
         items: [
             {
-                title: 'Item 1'
+                id: 1,
+                title: 'Item 1',
+                price: 9.95
             },
             {
-                title: 'Item 2'
+                id: 2,
+                title: 'Item 2',
+                price: 4.99
             },
             {
-                title: 'Item 3'
+                id: 3,
+                title: 'Item 3',
+                price: 12.95
             }
         ],
         cart: []
     },
     methods: {
-        addItem: function(index) {
-            // todo: check if item is already on cart
-            this.total += 9.99;
-            this.cart.push(this.items[index]);
+        addItem(index) {
+            var item = this.items[index],
+                exist = false;
+            this.total += item.price;
+            for(var i = 0; i < this.cart.length; i++) {
+                if(this.cart[i].id === item.id) {
+                    this.cart[i].amount++;
+                    exist = true;
+                    break;
+                }
+            }
+            if(!exist) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    price: item.price,
+                    amount: 1
+                });
+            }
+        },
+        increase(cartIndex) {
+            var item = this.cart[cartIndex];
+            item.amount++;
+            this.total += item.price;
+        },
+        decrease(cartIndex) {
+            var item = this.cart[cartIndex];
+            item.amount--;
+            this.total -= item.price;
+            if(item.amount <= 0) {
+                this.cart.splice(cartIndex, 1);
+            }
+        },
+        onSubmit(event) {
+
+        }
+    },
+    filters: {
+        currency: function (price) {
+            return price.toFixed(2) + '€'
         }
     }
 });
