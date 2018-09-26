c365fb8eae983665f05db6bec85491d8ea71f86b
Tobias Schramek <webtsofficial@gmail.com>
2018-09-26 12:21:27 +0200

    Added example items to the app and displayed them in the products section, added possibility to add them to card and show them in the card. Not yet profen if item already exist in cart.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,25 @@
 new Vue({
     el: '#app', // CSS Selektor String
     data: { // holds global data
-        total: 0
+        total: 0,
+        items: [
+            {
+                title: 'Item 1'
+            },
+            {
+                title: 'Item 2'
+            },
+            {
+                title: 'Item 3'
+            }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index) {
+            // todo: check if item is already on cart
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
+        }
     }
 });
